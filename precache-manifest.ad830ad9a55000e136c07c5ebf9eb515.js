a9fff6c17a7b4ca71a3a4a58389dfa0b36c6776f
surajraccha <surajraccha55@gmail.com>
2021-04-23 21:36:40 +0530

    Updates

---
+++ precache-manifest.ad830ad9a55000e136c07c5ebf9eb515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f182371a5749e841b89ad7b92307a189",
+    "url": "/test_repository/index.html"
+  },
+  {
+    "revision": "562105ba3ea779fa63dd",
+    "url": "/test_repository/static/css/main.cb669b7f.chunk.css"
+  },
+  {
+    "revision": "3493a3fffc2c1c1eb51c",
+    "url": "/test_repository/static/js/2.408a8e36.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/test_repository/static/js/2.408a8e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "562105ba3ea779fa63dd",
+    "url": "/test_repository/static/js/main.349a60cc.chunk.js"
+  },
+  {
+    "revision": "f9d9f6c07583303eafb4",
+    "url": "/test_repository/static/js/runtime-main.dd352981.js"
+  }
+]);
